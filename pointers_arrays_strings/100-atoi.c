979652cd713b43ea8460e0927b81bbaddd662556
saraum1 <saaraaaallls1@gmail.com>
2025-09-03 11:34:22 +0000

    Fix 100-atoi: handle signs correctly and ensure Betty compliance

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -9,39 +8,29 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, started = 0;
+	int i = 0, sign = 1;
 	int acc = 0;
 
-	while (s[i] != '\0' && !started)
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] == '+')
 			;
-		else if (s[i] >= '0' && s[i] <= '9')
-			started = 1;
-		if (!started)
-			i++;
+		else if (s[i] == ' ')
+			;
+		i++;
 	}
-	if (!started)
-		return (0);
 
 	while (s[i] >= '0' && s[i] <= '9')
 	{
 		int d = s[i] - '0';
 
 		if (sign > 0)
-		{
-			if (acc > (INT_MAX - d) / 10)
-				return (INT_MAX);
 			acc = acc * 10 + d;
-		}
 		else
-		{
-			if (acc < (INT_MIN + d) / 10)
-				return (INT_MIN);
 			acc = acc * 10 - d;
-		}
+
 		i++;
 	}
 	return (acc);
