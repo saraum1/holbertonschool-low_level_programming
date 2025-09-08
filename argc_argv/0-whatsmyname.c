2b27b14101ff36d3241f9b0c7f6753ed479db6d0
saraum1 <saaraaaallls1@gmail.com>
2025-09-08 11:37:43 +0000

    Fix task 0: update 0-whatsmyname.c to print program name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints the program name
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - prints the program name (including its path), followed by a new line
+ * @argc: argument count (unused)
+ * @argv: array of argument strings; argv[0] is the program name/path
  *
- * Return: 0
+ * Return: 0 on success
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-    printf("%s\n", argv[0]);
-    return (0);
+	(void)argc; /* silence unused-parameter warning */
+
+	printf("%s\n", argv[0]);
+	return (0);
 }
 
