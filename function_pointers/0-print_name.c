7b0393787a8dcb22dcbbe42d998512373769d50c
saraum1 <saaraaaallls1@gmail.com>
2025-09-22 12:33:42 +0000

    Add task 0 - print_name with Betty style and README

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,18 +1,18 @@
 #include "function_pointers.h"
-#include <stddef.h> /* for NULL */
+#include <stddef.h>
 
 /**
  * print_name - prints a name using a function pointer
  * @name: string to print
- * @f: pointer to function that prints
+ * @f: pointer to function that prints a string
  *
- * Return: Nothing
+ * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name == NULL || f == NULL)
-        return;
+	if (name == NULL || f == NULL)
+		return;
 
-    f(name);
+	f(name);
 }
 
