f53b18e096a0e34f014fcd3cd0825e537968d0a5
saraum1 <saraum1@users.noreply.github.com>
2025-10-16 14:05:42 +0000

    3-cp: check read() before opening dst; fixed Betty long lines

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,18 +13,31 @@ void error_exit(int code, const char *msg, const char *arg)
 }
 
 /**
- * copy_file - copies the content of one file to another
- * @file_from: name of the source file
- * @file_to: name of the destination file
+ * copy_file - copies content of one file to another
+ * @file_from: source file name
+ * @file_to: destination file name
+ *
+ * Note:
+ * Read once before opening destination to ensure
+ * read-failure returns 98 even if file_to is invalid.
  */
 void copy_file(const char *file_from, const char *file_to)
 {
-	int fd_from, fd_to, r, w;
+	int fd_from, fd_to;
+	ssize_t r, w;
 	char buf[1024];
 
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 		error_exit(98, "Error: Can't read from file %s\n", file_from);
+
+	/* First read to validate readability before touching file_to */
+	r = read(fd_from, buf, 1024);
+	if (r == -1)
+	{
+		close(fd_from);
+		error_exit(98, "Error: Can't read from file %s\n", file_from);
+	}
 
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
@@ -33,18 +46,9 @@ void copy_file(const char *file_from, const char *file_to)
 		error_exit(99, "Error: Can't write to %s\n", file_to);
 	}
 
-	while (1)
+	/* Write the first block (if any), then loop */
+	while (r > 0)
 	{
-		r = read(fd_from, buf, 1024);
-		if (r == -1)
-		{
-			close(fd_from);
-			close(fd_to);
-			error_exit(98, "Error: Can't read from file %s\n", file_from);
-		}
-		if (r == 0)
-			break;
-
 		w = write(fd_to, buf, r);
 		if (w == -1 || w != r)
 		{
@@ -52,22 +56,32 @@ void copy_file(const char *file_from, const char *file_to)
 			close(fd_to);
 			error_exit(99, "Error: Can't write to %s\n", file_to);
 		}
+
+		r = read(fd_from, buf, 1024);
+		if (r == -1)
+		{
+			close(fd_from);
+			close(fd_to);
+			error_exit(98, "Error: Can't read from file %s\n", file_from);
+		}
 	}
 
 	if (close(fd_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		dprintf(STDERR_FILENO,
+			"Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
 	if (close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		dprintf(STDERR_FILENO,
+			"Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
 }
 
 /**
- * main - entry point, handles arguments and calls copy_file
+ * main - entry point: validates args and calls copy_file
  * @argc: argument count
  * @argv: argument vector
  *
@@ -77,11 +91,11 @@ int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO,
+			"Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
 	copy_file(argv[1], argv[2]);
 	return (0);
 }
-
