eccc96670b404564449eebea546a9d82603edb8e
saraum1 <saaraaaallls1@gmail.com>
2025-10-25 16:37:51 +0000

    Fixed 6-hash_table_delete.c and 6-main.c formatting for Betty

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,31 +1,30 @@
 #include "hash_tables.h"
-#include <stdlib.h>
 
 /**
  * hash_table_delete - Deletes a hash table
- * @ht: The hash table to delete
+ * @ht: The hash table
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t *node, *tmp;
-    unsigned long int i;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
 
-    if (!ht)
-        return;
+	if (!ht)
+		return;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        node = ht->array[i];
-        while (node)
-        {
-            tmp = node->next;
-            free(node->key);
-            free(node->value);
-            free(node);
-            node = tmp;
-        }
-    }
-    free(ht->array);
-    free(ht);
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
 
