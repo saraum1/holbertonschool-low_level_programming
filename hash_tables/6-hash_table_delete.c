bd7f59963dff1f5286d0ec2e67333d214fc095df
saraum1 <saaraaaallls1@gmail.com>
2025-10-25 16:27:05 +0000

    Add 6-hash_table_delete.c to delete hash table properly

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+    hash_node_t *node, *tmp;
+    unsigned long int i;
+
+    if (!ht)
+        return;
+
+    for (i = 0; i < ht->size; i++)
+    {
+        node = ht->array[i];
+        while (node)
+        {
+            tmp = node->next;
+            free(node->key);
+            free(node->value);
+            free(node);
+            node = tmp;
+        }
+    }
+    free(ht->array);
+    free(ht);
+}
+
