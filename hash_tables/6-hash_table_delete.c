7bd7d904ca173d88787b549652af1ee64856d113
saraum1 <saaraaaallls1@gmail.com>
2025-10-26 15:56:03 +0000

    Fix 6-hash_table_delete.c: free all nodes, Betty style

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,30 +1,32 @@
 #include "hash_tables.h"
+#include <stdlib.h>
 
 /**
- * hash_table_delete - Deletes a hash table
- * @ht: The hash table
+ * hash_table_delete - Deletes a hash table and frees all memory
+ * @ht: Pointer to the hash table to delete
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *node, *tmp;
-	unsigned long int i;
+    unsigned long int i;
+    hash_node_t *node, *tmp;
 
-	if (!ht)
-		return;
+    if (ht == NULL)
+        return;
 
-	for (i = 0; i < ht->size; i++)
-	{
-		node = ht->array[i];
-		while (node)
-		{
-			tmp = node->next;
-			free(node->key);
-			free(node->value);
-			free(node);
-			node = tmp;
-		}
-	}
-	free(ht->array);
-	free(ht);
+    for (i = 0; i < ht->size; i++)
+    {
+        node = ht->array[i];
+        while (node)
+        {
+            tmp = node->next;
+            free(node->key);
+            free(node->value);
+            free(node);
+            node = tmp;
+        }
+    }
+
+    free(ht->array);
+    free(ht);
 }
 
